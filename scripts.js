9cecb11b12faab84826d58e3dd17aa66a388546a
JIHUNCAR <86134322+car8411@users.noreply.github.com>
2025-01-05 20:50:03 +0900

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,11 +1,9 @@
-function showContent(sectionId) {
+function showSection(sectionId) {
     // 모든 섹션 숨기기
     const sections = document.querySelectorAll('.content');
-    sections.forEach(section => {
-        section.classList.remove('active');
-    });
+    sections.forEach(section => section.classList.remove('active'));
 
-    // 클릭된 섹션만 보이도록 설정
+    // 선택된 섹션만 활성화
     const activeSection = document.getElementById(sectionId);
     activeSection.classList.add('active');
 }
